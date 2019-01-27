c0bc2699d321ed8c5026697edeaef58875b91e97
igor <igoratakhanov@gmail.com>
2019-01-27 15:25:20 -0500

    seed and migratoins working, pong and error test passing. TODO: write more tests, write more routes

---
+++ services/locations/src/db/seeds/locations.js
@@ -0,0 +1,17 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex("locations")
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return Promise.join(
+        knex("locations").insert({
+          user_id: 1,
+          lat: 40.7128,
+          long: 74.006
+        })
+      ).catch(err => {
+        console.log(err);
+      });
+    });
+};
